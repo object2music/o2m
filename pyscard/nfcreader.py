de5b44601a9e53b59c1b176db7706c8e40b87204
Antoine Josnin <antoine@akago.fr>
2020-03-06 18:44:44 +0100

    try catch on no card exceptions

--- pyscard/nfcreader.py
+++ pyscard/nfcreader.py
@@ -5,6 +5,7 @@ from smartcard.CardMonitoring import CardMonitor, CardObserver
 from smartcard.util import toHexString
 from smartcard.System import readers
 from smartcard.scard import SCardEstablishContext, SCardTransmit, SCardConnect, SCARD_PROTOCOL_T1, SCARD_PROTOCOL_T0, SCARD_SHARE_SHARED, SCARD_S_SUCCESS, SCARD_SCOPE_USER
+from smartcard.Exceptions import NoCardException
 
 from events import Events
 
@@ -29,15 +30,20 @@ class PrintObserver(CardObserver):
     def update(self, observable, actions):
         (addedcards, removedcards) = actions
         for card in addedcards:
-            # Methode 1
-            readerObject = self.get_reader_by_name(card.reader)
-            uid_str = self.get_id_with_reader(readerObject)
-            print('METHODE 1 : Card id : {} in reader : {}'.format(uid_str, readerObject))
 
-            # Methode 2
+            # Methode 1
             int_id = self.get_id(card.reader)
             card.id = self.convert_to_hex_as_string(int_id)
-            print("METHODE 2 : +Inserted: {} in reader : {}".format(card.id, card.reader))
+            print("METHODE 1 : +Inserted: {} in reader : {}".format(card.id, card.reader))
+
+            # Methode 2
+            readerObject = self.get_reader_by_name(card.reader)
+            uid_str = self.get_id_with_reader(readerObject)
+            if uid_str != None:
+                print('METHODE 2 : Card id : {} in reader : {}'.format(uid_str, readerObject))
+            else:
+                print('METHODE 2 : ERROR')
+
 
         for card in removedcards:
             print("-Removed: {} from reader : {}".format(card.id, card.reader))
@@ -59,17 +65,21 @@ class PrintObserver(CardObserver):
         return self.launch_command_with_reader(reader, cmdMap['getuid'])
 
     def launch_command_with_reader(self, reader, command):
-        connection = reader.createConnection()
-        connection.connect()
-        data, sw1, sw2 = connection.transmit(command)
-        return self.convert_to_hex_as_string(data)
+        try:
+            connection = reader.createConnection()
+            connection.connect()
+            data, sw1, sw2 = connection.transmit(command)
+            return self.convert_to_hex_as_string(data)
+        except NoCardException as err: 
+            print(err)
+            return None
 
     '''
         Première méthode de connexion à la carte
         Le nom du reader suffit mais renvoie plus de donnée que nécessaire (obligé de tronquer le résultat)
     '''    
     def get_id(self, reader_name):
-        return self.launch_command(reader_name, cmdMap['getuid'])[:4] # we keep only the fourth first elements of the response
+        return self.launch_command(reader_name, cmdMap['getuid'])
 
     def launch_command(self, reader_name, command):
         hresult, hcontext = SCardEstablishContext(SCARD_SCOPE_USER)
@@ -83,7 +93,8 @@ class PrintObserver(CardObserver):
 
         # hresult, response = SCardTransmit(hcard,dwActiveProtocol,[0xFF,0xCA,0x00,0x00,0x00])
         hresult, response = SCardTransmit(hcard,dwActiveProtocol, command)
-        return response
+        
+        return response[:len(response)-2]
 
     def convert_to_hex_as_string(self, data):
         hexData = [format(i, 'X').zfill(2) for i in data] # we convert to hex with format and add a 0 digit if necessary
@@ -102,7 +113,7 @@ class NfcReader():
         self.cardmonitor.addObserver(self.cardobserver)
         self.cardobserver.events.on_change += self.update_change
         
-        self.mute_all_readers()
+        # self.mute_all_readers()
         
         self.events.on_init()
 
