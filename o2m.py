534d3d1374c7596549e03cf83ae9f56b8631c01b
Antoine Josnin <antoine@akago.fr>
2020-03-07 12:32:20 +0000

    code refactoring and comments

--- o2m.py
+++ o2m.py
@@ -11,6 +11,16 @@ class NfcToMopidy():
         
     def get_new_cards(self, addedCards, removedCards, activeCards):
         self.activecards = activeCards
+        self.pretty_print_nfc_data(addedCards, removedCards)
+
+        # Put some timestamps on cards ?
+        
+        # Do some stuff in sqlite database
+
+        # Launch some commands to mopidy
+
+
+    def pretty_print_nfc_data(self, addedCards, removedCards):
         print('-------')
         print('NFC TAGS CHANGED!')
         print('COUNT : \n     ADDED : {}  \n     REMOVED : {} '.format(len(addedCards), len(removedCards)))
@@ -23,7 +33,6 @@ class NfcToMopidy():
         for key, card in self.activecards.items():
             print('     Reader : {} with card : {} '.format(key, card.id))
         print('-------')
-
 
 if __name__ == "__main__":
     nfcHandler = NfcToMopidy()
