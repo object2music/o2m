e8ff30184bd93722ec0437276b03e171f949dc71
Patrick Vincent <pvincent@grandlyon.com>
2023-09-17 23:36:04 +0200

    Excluding /r in data line

--- src/o2mtomopidy.py
+++ src/o2mtomopidy.py
@@ -516,6 +516,7 @@ class O2mToMopidy:
         # Looping on hybrid playlist (delimited by \n)
         data = box.data.split("\n")
         data = [x for x in data if not x.startswith('#')]
+        data = [x for x in data if not x.startswith('\r')]
 
         for content in data:
             # Recommandation
