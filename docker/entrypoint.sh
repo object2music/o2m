1a62387c33aae6642f42410b256c80913c5a225b
Patrick Vincent <pvincent@grandlyon.com>
2023-09-21 22:30:07 +0200

    Sed rights issue

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,8 +8,8 @@ cp index.html /usr/local/lib/python3.10/dist-packages/mopidy_iris/static/index.h
 # Get env vars
 O2M_API_PORT=${O2M_API_PORT:-5000}
 O2M_BACKOFFICE_URI=${O2M_BACKOFFICE_URI:-http://localhost:5011}
-sed -i "s/:6691\/api\//:$O2M_API_PORT\/api\//g" /usr/local/lib/python3.10/dist-packages/mopidy_iris/static/o2m.js
-sed -i "s/http:\/\/localhost:5011:$O2M_BACKOFFICE_URI/g" /usr/local/lib/python3.10/dist-packages/mopidy_iris/static/o2m.js
+sed -ci "s/:6691\/api\//:$O2M_API_PORT\/api\//g" /usr/local/lib/python3.10/dist-packages/mopidy_iris/static/o2m.js
+sed -ci "s/http:\/\/localhost:5011:$O2M_BACKOFFICE_URI/g" /usr/local/lib/python3.10/dist-packages/mopidy_iris/static/o2m.js
 
 #Create Mysql
 #rm -rf mysql_data
