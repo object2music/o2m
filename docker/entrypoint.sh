a9b4d1d357295509e33c9bbe3ea7cd652c232d11
Patrick Vincent <pvincent@grandlyon.com>
2023-09-26 21:44:25 +0200

    Entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,9 +7,9 @@ cp index.html /usr/local/lib/python3.10/dist-packages/mopidy_iris/static/index.h
 
 # Get env vars
 O2M_API_PORT=${O2M_API_PORT:-5000}
+sed -i "s/:6691\/api\//:$O2M_API_PORT\/api\//g" /usr/local/lib/python3.10/dist-packages/mopidy_iris/static/o2m.js
 O2M_BACKOFFICE_URI=${O2M_BACKOFFICE_URI:-http://localhost:5011}
-sed -ci "s/:6691\/api\//:$O2M_API_PORT\/api\//g" /usr/local/lib/python3.10/dist-packages/mopidy_iris/static/o2m.js
-sed -ci "s/http:\/\/localhost:5011:$O2M_BACKOFFICE_URI/g" /usr/local/lib/python3.10/dist-packages/mopidy_iris/static/o2m.js
+sed -i "s/http:\/\/localhost:5011:$O2M_BACKOFFICE_URI/g" /usr/local/lib/python3.10/dist-packages/mopidy_iris/static/o2m.js
 
 #Create Mysql
 #rm -rf mysql_data
