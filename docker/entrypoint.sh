472c0c1678ed599c9362592f9c9e35e91fc46933
Patrick VINCENT <pvincent4@gmail.com>
2023-11-03 12:16:17 +0100

    Fix [Docker] Containers dissociation

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,13 +6,14 @@ bash create_conf_files.sh
 cp index.html /usr/local/lib/python3.10/dist-packages/mopidy_iris/static/index.html
 
 # Get env vars
-O2M_API_PORT=${O2M_API_PORT:-5000}
+O2M_API_PORT=${PORT_O2M_API:-5000}
 O2M_BACKOFFICE_URI=${O2M_BACKOFFICE_URI:-'http://localhost:5011'}
+
 sed -i "s/:6691\/api\//:$O2M_API_PORT\/api\//g" /usr/local/lib/python3.10/dist-packages/mopidy_iris/static/o2m.js
 sed -i "s/http:\/\/localhost:5011/$O2M_BACKOFFICE_URI/g" /usr/local/lib/python3.10/dist-packages/mopidy_iris/static/o2m.js
 
-# Get env vars
 <<com
+# Get env vars
 FILE=tmp_o2m.js
 if test -f "$FILE"; then
     rm tmp_o2m.js
@@ -31,7 +32,6 @@ sed "s/http:\/\/localhost:5011/$O2M_BACKOFFICE_URI/g" /usr/local/lib/python3.10/
 cp tmp_o2m1.js /usr/local/lib/python3.10/dist-packages/mopidy_iris/static/o2m.js
 rm tmp_o2m1.js
 com
-
 #Create Mysql
 #rm -rf mysql_data
 
