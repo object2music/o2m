af0b661ebb71154845eacf07165c87a4f92397e0
Patrick Vincent <pvincent4@gmail.com>
2023-12-08 23:55:58 +0100

    Update conf

--- o2m/main.py
+++ o2m/main.py
@@ -47,8 +47,8 @@ if __name__ == "__main__":
     while True:
         strer = 1
         try:
-            mopidy = MopidyAPI(host='mopidy', port=6680)
-            #mopidy = MopidyAPI(host='localhost', port=o2mConf["o2m"]["port_mopidy"])
+            #mopidy = MopidyAPI(host='mopidy', port=6680)
+            mopidy = MopidyAPI(host=o2mConf["o2m"]["host_mopidy"], port=o2mConf["o2m"]["port_mopidy"])
             #mopidy = MopidyAPI(host='51.15.205.150', port='6680')
             #mopidy = MopidyAPI()
             o2mHandler = O2mToMopidy(mopidy, o2mConf, mopidyConf, logging)
