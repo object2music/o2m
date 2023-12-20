7286ca5d4d9c5aaf1a1027a9828e95ed83fc7665
yassin@siouda.com <yassin@siouda.com>
2023-12-20 14:47:19 +0100

    chore(backend): created stats_played schema

---
+++ backend/pb_migrations/1703079987_updated_users.js
@@ -0,0 +1,56 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  collection.options = {
+    "allowEmailAuth": true,
+    "allowOAuth2Auth": true,
+    "allowUsernameAuth": true,
+    "exceptEmailDomains": null,
+    "manageRule": null,
+    "minPasswordLength": 8,
+    "onlyEmailDomains": null,
+    "onlyVerified": false,
+    "requireEmail": false
+  }
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "x51n30ob",
+    "name": "stats_played",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "bf49rbpqbz1aeij",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": null,
+      "displayFields": null
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  collection.options = {
+    "allowEmailAuth": true,
+    "allowOAuth2Auth": true,
+    "allowUsernameAuth": true,
+    "exceptEmailDomains": null,
+    "manageRule": null,
+    "minPasswordLength": 8,
+    "onlyEmailDomains": null,
+    "requireEmail": false
+  }
+
+  // remove
+  collection.schema.removeField("x51n30ob")
+
+  return dao.saveCollection(collection)
+})
