c91b3201f4f8bf321b1960788603cee37317fb98
Patrick Vincent <pvincent4@gmail.com>
2026-01-08 22:13:16 +0100

    Fix Podcasts_unfinished

--- o2m/src/dbhandler.py
+++ o2m/src/dbhandler.py
@@ -217,7 +217,7 @@ class DatabaseHandler():
         date_now = datetime.datetime.utcnow().timestamp()
         #query = Stats.select().where( ((Stats.uri % '%podcast+%') | (Stats.uri % '%youtube:video%')| (Stats.uri % '%yt:%'))& (Stats.read_end <= 0.9)& (Stats.read_position > 30000)& (Stats.option_type != "info")& (Stats.option_type != "normal")).order_by(Stats.last_read_date.desc()).limit(limit)
         #query = Stats.select().where( ((Stats.uri % '%podcast+%') | (Stats.uri % '%youtube:video%')| (Stats.uri % '%yt:%'))
-        query = Stats.select().where( ((Stats.uri % '%podcast+%')| (Stats.uri % '%youtube:video%')| (Stats.uri % '%yt:%'))& (Stats.read_end < 0.9)& (Stats.read_position > 30000)
+        query = Stats.select().where( ((Stats.uri % '%podcast+%')| (Stats.uri % '%youtube:video%')| (Stats.uri % '%yt:%'))& (Stats.read_end < 0.9)& (Stats.read_position > 0)& (Stats.read_count_end == 0)
             & (
             #((Stats.option_type != "info")& (Stats.option_type != "normal")& (Stats.read_count_end <= discover_level/2) & (Stats.skipped_count <= discover_level))
             ((Stats.option_type != "normal")& (Stats.read_count_end <= discover_level/2) & (Stats.skipped_count <= discover_level))
